ea6be6ceda026ef7d188ff53f610081279dceb81
Matthew Ogtong <ogtongm@gmail.com>
2020-07-30 17:46:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function add(first, second) {
   return first + second
 }
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
